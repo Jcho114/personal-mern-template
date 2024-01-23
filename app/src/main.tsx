109045b3e3e079c6922c2ba2ed1480e1d45fca88
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-01-23 13:06:43 -0500

    added react query demo

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>} />
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App/>} />
+        </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 )
